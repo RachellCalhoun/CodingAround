264abc148388fdd5f333c0fef57194144c39ec6e
Rachell Calhoun <chellybea@hotmail.com>
2015-04-21 19:22:58 +0900

    reorganizing

--- countingstrings.py
+++
@@ -1,6 +0,0 @@
-s = "ajlsdkjflsboblaksdj;flksjbob"
-totalBobs = 0
-for letter in range(1, len(s)-1):
-    if s[letter-1:letter+2] == 'bob':
-        totalBobs += 1
-print 'Number of times bob occurs is:', totalBobs
